348b2fb088addd59d03c9c5072aebe43b0dbfa65
Akshay Jadhav <akshayjadhav461999@gmail.com>
2024-06-18 20:44:08 +0530

    Deleted Web File (Maybe later)

--- src/NativeMenusView.web.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-import { NativeMenusViewProps } from "./NativeMenus.types";
-
-export default function NativeMenusView(props: NativeMenusViewProps) {
-  return <div>{/* <span>{props.name}</span> */}</div>;
-}
